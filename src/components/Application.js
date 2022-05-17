2a0a47958893e36403b40a100d31a0c01d5a8544
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-17 14:34:22 -0400

    Add state to the app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+import DayList from "components/DayList";
+
+const days = [
+  { id: 1, name: "Monday", spots: 2 },
+  { id: 2, name: "Tuesday", spots: 5 },
+  { id: 3, name: "Wednesday", spots: 0 },
+];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -20,7 +29,11 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+        />
       </section>
     </main>
   );
