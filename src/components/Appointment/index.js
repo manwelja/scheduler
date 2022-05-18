e563ae452c81b8fe0a6ba080205addceb90960cb
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-18 13:54:50 -0400

    Add Empty and Header components and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
 
--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index.js";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -128,4 +130,6 @@ storiesOf("InterviewerListItem", module)
       backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Time", () => <Appointment time="12pm" />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
