d52b3128f9184ffe2f8abbd58e308688f0cb5a34
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-18 14:40:29 -0400

    Build the appointment form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./styles.scss";
+
+import InterviewerList from "../InterviewerList";
+import Button from "../Button";
+
+export default function Form(props) {
+
+  const { student, interviewer, interviewers, onSave, onCancel } = props;
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+              your code goes here
+            */
+          />
+        </form>
+        <InterviewerList 
+          value={ interviewer }
+          interviewers={ interviewers }
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={ onCancel }>Cancel</Button>
+          <Button confirm onClick={ onSave }>Save</Button>
+        </section>
+      </section>
+    </main>
+  ); 
+
+}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import Error from "./Error";
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Form from "./Form";
 
 export default function Appointment(props) {
 
