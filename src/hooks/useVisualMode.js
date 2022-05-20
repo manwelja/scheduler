bdbc114620937bb2bbf0f9df6b7f09dad7427d22
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-20 10:41:08 -0400

    Implement the useVisualMode function

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,20 @@
+import react, { useState, useEffect } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (tMode, replaceFlag = false)  => {
+    setMode(tMode);
+    //if replaceFlag is true, remove the last state from history then add the new one, otherwise just add the new state to the history array
+    replaceFlag ?  setHistory([...history.slice(0, history.length-1), tMode]) : setHistory([...history, tMode]);
+  }  
+  
+  //Set the new mode to the previous one in the history, then remove the previous "last" state from teh history array
+  const back = () => {
+    setMode(history[history.length-2])
+    setHistory(history.slice(0, history.length-1))
+  };
+
+  return { mode, transition, back };
+}
